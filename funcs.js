0522f8ca1f4624d106ed330f000a92ea053c7acb
Tanzil <mtanzilislam1997@gmail.com>
2022-10-19 15:50:27 +0100

    done

--- funcs.js
+++ funcs.js
@@ -1,13 +1,27 @@
 function isFive(num) {
   // Your code here
+  return num === 5
 }
 
 function isOdd(number) {
   // Your code here
+  if(typeof number !== `number`) {
+    throw new Error('You can only input a number');
+  }
+
+  if(number % 2 === 0) {
+    return false
+  } else {
+  return true}
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  let arr = []
+  for(let i = min; i <= max; i += step) {
+        arr.push(i)
+  }
+  return arr
 }
 
 
--- test/class-spec.js
+++ test/class-spec.js
@@ -4,31 +4,57 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+
+  let word;
+  let word2;
+
+  beforeEach(() => {
+    word = new Word('Appletini');
+    word2 = new Word('Crazytown');
+  });
+
+describe("Word", function () {
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(word).to.have.all.keys('word');
+      expect(word2).to.have.all.keys('word');
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word).to.have.property('word', 'Appletini');
+      expect(word2).to.have.property('word', 'Crazytown');
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      const consonantWord = word.removeVowels();
+      const consonantWord2 = word2.removeVowels();
+
+      expect(consonantWord).to.equal('ppltn');
+      expect(consonantWord2).to.equal('Crzytwn');
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+
+      const vowelsWord = word.removeConsonants();
+      const vowelsWord2 = word2.removeConsonants();
+
+      expect(vowelsWord).to.equal('Aeii');
+      expect(vowelsWord2).to.equal('ao');
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      const plWord = word.pigLatin();
+      const plWord2 = word2.pigLatin();
+
+      expect(plWord).to.equal('Appletiniyay');
+      expect(plWord2).to.equal('azytownCray');
     });
   });
 });
+})
